e09db8a1789a99ccbd2fb85f1dfa107a6037ad67
leona-art <leonagraph@gmail.com>
2024-06-07 12:43:40 +0900

    Revert "[wip]"
    This reverts commit eecc34a0df064acac99f1a0fca335bf3f2ebcd72.

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,48 +1,25 @@
 import { Title } from "@solidjs/meta";
-import { createAsync, redirect,revalidate, action } from "@solidjs/router";
-import { createEffect, onMount } from "solid-js";
+import { createAsync,cache } from "@solidjs/router";
+import { onMount } from "solid-js";
 import Counter from "~/components/Counter";
-import { Button } from "~/components/ui/button";
 
 
-const getArticles = async () => {
+const getSample = async () => {
   "use server"
   const url = import.meta.env.VITE_BACKEND_URL as string
   const res = await fetch(`${url}/articles`)
   const articles = await res.json() as string
-  return articles
+  return import.meta.env.VITE_BACKEND_URL as string
 }
-const postArticleAction = action(async (title: string, content: string) => {
-  "use server"
-  console.log(title, content)
-  const url = import.meta.env.VITE_BACKEND_URL as string
-  // const title = formData.get("title") as string
-  // const content = formData.get("content") as string
-  const res = await fetch(
-    `${url}/article/1`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, content }),
-    }
-  )
-  const article = await res.json()
-  
-  throw redirect("/")
-})
 export default function Home() {
-  const sample = createAsync(() => getArticles())
-  createEffect(() => {
+  const sample = createAsync(() => getSample())
+  onMount(() => {
     console.log(sample())
   })
   return (
     <main>
       <div>
-        <form action={postArticleAction.with("title","content")} method="post">
-          <Button type="submit">Submit</Button>
-        </form>
+        {sample()}
       </div>
     </main>
   );
